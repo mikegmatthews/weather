ebc416609ba00a70a2309140bba538c9f38e999e
Mike Matthews <michael.g.matthews5@gmail.com>
2022-10-06 12:03:46 -0400

    Added application name

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Weather.MixProject do
   def project do
     [
       app: :weather,
+      name: "NOAA Weather Data",
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
